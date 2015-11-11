7400498ca51ea65b60ad1d51b82f264c101794ed
Manolo Carrasco <manolo@apache.org>
2015-11-11 02:35:47 +0100

    Step4 - Adding toolbars and drawer-toggle button

--- src/main/java/org/gwtcon/client/Chat.java
+++ src/main/java/org/gwtcon/client/Chat.java
@@ -10,10 +10,16 @@ public class Chat implements com.google.gwt.core.client.EntryPoint {
     public void onModuleLoad() {
         Polymer.importHref(Arrays.asList(
                 "paper-styles",
+                "iron-icons/iron-icons.html",
                 "neon-animation/neon-animations.html",
                 "paper-drawer-panel",
                 "paper-header-panel",
-                "iron-flex-layout"
+                "paper-scroll-header-panel",
+                "paper-toolbar",
+                "paper-icon-button",
+                "paper-button",
+                "iron-flex-layout",
+                "iron-icon"
         ));
 
         Polymer.whenReady((o) -> {
